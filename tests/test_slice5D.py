155740f839d0516486a67e3b0633855481e05125
Tomaz Vieira <tom.fogaca@gmail.com>
2019-07-03 15:29:03 +0200

    Adds slice clamp test

--- tests/test_slice5D.py
+++ tests/test_slice5D.py
@@ -31,3 +31,18 @@ def test_slice_does_not_contain_translated_slice():
     slc = Slice5D(x=slice(10,100), y=slice(20,200), z=0, t=0, c=0)
     translated_slc = slc.translated(Point5D.zero(x=10))
     assert not slc.contains(translated_slc)
+
+def test_slice_clamp():
+    outer = Slice5D(x=slice(10,100), y=slice(20,200))
+    inner = Slice5D(x=slice(20, 50), y=slice(30, 40), z=0, t=0, c=0)
+    assert outer.clamped(inner) == inner
+    assert inner.clamped(outer) == inner
+
+    intersecting_outer = Slice5D(x=slice(50,200), y=slice(30, 900))
+    assert intersecting_outer.clamped(outer) == Slice5D(x=slice(50, 100), y=slice(30, 200))
+
+    intersecting_outer = Slice5D(x=slice(-100,50), y=slice(10, 100))
+    assert intersecting_outer.clamped(outer) == Slice5D(x=slice(10, 50), y=slice(20, 100))
+
+    outside_outer = Slice5D(x=slice(200,300), y=slice(400,500))
+    assert outside_outer.clamped(outer).defined_with(Shape5D()).shape.volume == 0
