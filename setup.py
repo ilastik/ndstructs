a8fb5cbf00fa3c45c08ff75b77129de4243e040f
k-dominik <k-dominik@users.noreply.github.com>
2019-07-05 09:45:17 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ndstructs",
+    version="0.0.1",
+    author="Tomaz Vieira",
+    author_email="team@ilastik.org",
+    license="MIT",
+    description="Short description",
+    packages=["ndstructs", "ndstructs.utils"],
+    install_requires=["numpy", "pillow"],
+)
