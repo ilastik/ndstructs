350bc9e18019f0433fc95a6b323b462597d369c2
k-dominik <k-dominik@users.noreply.github.com>
2019-07-05 10:23:21 +0200

    added bumpversion

--- ndstructs/__init__.py
+++ ndstructs/__init__.py
@@ -1,3 +1,5 @@
 from .array5D import Array5D
 from .array5D import Image, ScalarImage, LinearData, ScalarData, ScalarLine, StaticLine
 from .point5D import Point5D, Shape5D, Slice5D
+
+__version__ = "0.0.1dev0"
