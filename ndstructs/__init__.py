866b1036b899c1aa8d6ecb54f983fe9bffc79215
Tomaz Vieira <tom.fogaca@gmail.com>
2020-01-31 16:14:47 +0100

    bumps version

--- ndstructs/__init__.py
+++ ndstructs/__init__.py
@@ -2,4 +2,4 @@ from .array5D import Array5D
 from .array5D import Image, ScalarImage, LinearData, ScalarData, ScalarLine, StaticLine
 from .point5D import Point5D, Shape5D, Slice5D
 
-__version__ = "0.0.1"
+__version__ = "0.0.2dev0"
