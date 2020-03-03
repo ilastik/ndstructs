3180c62ee8b0b39b90a58913e51d28e8980ca26b
Tomaz Vieira <tom.fogaca@gmail.com>
2020-03-03 14:55:08 +0100

    Uses fixtures in test_datasink

--- tests/test_datasink.py
+++ tests/test_datasink.py
@@ -4,29 +4,35 @@ import pytest
 import numpy as np
 
 from ndstructs import Point5D, Slice5D, Array5D, Shape5D
-from ndstructs.datasource.DataSource import ArrayDataSource
+from ndstructs.datasource.DataSource import ArrayDataSource, DataSource
 from ndstructs.datasource.DataSourceSlice import DataSourceSlice
 from ndstructs.datasource.N5DataSource import N5DataSource
 from ndstructs.datasink import N5DataSink
 
 
-def test_n5_datasink(tmp_path: Path):
-    data = Array5D(np.arange(20 * 10 * 7).reshape(20, 10, 7), axiskeys="xyz")
-    ds = ArrayDataSource(data=data)
+@pytest.fixture
+def data() -> Array5D:
+    return Array5D(np.arange(20 * 10 * 7).reshape(20, 10, 7), axiskeys="xyz")
+
+
+@pytest.fixture
+def datasource(data: Array5D):
+    return ArrayDataSource(data=data)
+
+
+def test_n5_datasink(tmp_path: Path, data: Array5D, datasource: DataSource):
     dataset_path = tmp_path / "test_n5_datasink.n5/data"
-    sink = N5DataSink(path=dataset_path, data_slice=DataSourceSlice(ds), tile_shape=Shape5D(x=10, y=10))
+    sink = N5DataSink(path=dataset_path, data_slice=DataSourceSlice(datasource), tile_shape=Shape5D(x=10, y=10))
     sink.process(Slice5D.all())
 
     n5ds = N5DataSource(dataset_path)
     assert n5ds.retrieve(Slice5D.all()) == data
 
 
-def test_n5_datasink_saves_roi(tmp_path: Path):
-    data = Array5D(np.arange(20 * 10 * 7).reshape(20, 10, 7), axiskeys="xyz")
-    ds = ArrayDataSource(data=data)
-    roi = DataSourceSlice(ds, x=slice(5, 8), y=slice(2, 4))
+def test_n5_datasink_saves_roi(tmp_path: Path, data: Array5D, datasource: DataSource):
+    roi = DataSourceSlice(datasource, x=slice(5, 8), y=slice(2, 4))
 
-    dataset_path = tmp_path / "test_n5_datasink.n5/data"
+    dataset_path = tmp_path / "test_n5_datasink_saves_roi.n5/data"
     sink = N5DataSink(path=dataset_path, data_slice=roi, tile_shape=Shape5D(x=10, y=10))
     sink.process(Slice5D.all())
 
