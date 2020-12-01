9b3ddf413498b7b6381e04a01b0c4009ce73aaf2
Tomaz Vieira <tom.fogaca@gmail.com>
2020-12-01 13:34:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ndstructs",
-    version="0.0.4dev0",
+    version="0.0.5dev0",
     author="Tomaz Vieira",
     author_email="team@ilastik.org",
     license="MIT",
