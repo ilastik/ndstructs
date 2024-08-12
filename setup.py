25a309d8a7ee2f8c3cb7b51ecd592215f85b4ae8
k-dominik <k-dominik@users.noreply.github.com>
2024-08-12 16:25:58 +0200

    mnt: update dev deps, unpin pytest

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     packages=["ndstructs", "ndstructs.utils", "ndstructs.datasource", "ndstructs.datasink", "ndstructs.caching"],
     python_requires=">=3.8",
     install_requires=["numpy", "scikit-image", "h5py", "fs", "typing_extensions", "imageio"],
-    extras_require={"dev": ["pytest<5.4"]},
+    extras_require={"dev": ["pytest"]},
 )
